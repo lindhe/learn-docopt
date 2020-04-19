cb332bab95d6f21796922cb807c9239873eed6ed
Andreas Lindhé <andreas@lindhe.io>
2020-04-19 18:59:20 +0200

    Implement dynamic values in docstring

--- hello.py
+++ hello.py
@@ -4,13 +4,19 @@
 # License: MIT
 # Author: Andreas Lindhé
 
-"""Learn docopt.
+import sys
+from docopt import docopt
+
+dyn = "xkcd"
+
+docs = f"""Learn docopt.
 
 USAGE:
-hello.py <input_file> [options]
+{sys.argv[0]} <input_file> [options]
 
 OPTIONS:
   -b --bar BAR       Sets bar to BAR. [default: asdf]
+  --dynamic DYN      This is set dynamically. [default: {dyn}]
   -f --foo           Sets foo to true.
   -o --output FILE   Sets output file.
   -h --help          Show this help.
@@ -18,12 +24,10 @@ OPTIONS:
 
 """
 
-from docopt import docopt
-
 __author__ = "Andreas Lindhé"
 __license__ = "MIT"
 __version__ = "0.1.0"
 
 if __name__ == '__main__':
-  args = docopt(__doc__, version=__version__)
+  args = docopt(docs, version=__version__)
   print(args)
