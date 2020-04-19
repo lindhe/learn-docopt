b39e2a07c19e6d57c7e676de293ab364391a11d2
Andreas Lindhé <andreas@lindhe.io>
2020-04-19 17:54:23 +0200

    Add script stub

---
+++ docopt.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# License: MIT
+# Author: Andreas Lindhé
+
+import sys
+import docopt
+
+def main():
+  """ Main function description """
+  pass
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    sys.exit("\nInterrupted by ^C\n")
+
+
