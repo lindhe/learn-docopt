57eae13a680f2731c6484588722d1ecc7436d193
Andreas Lindhé <andreas@lindhe.io>
2020-04-19 18:55:33 +0200

    Setup something based on docstrings

--- hello.py
+++ hello.py
@@ -6,12 +6,24 @@
 
 """Learn docopt.
 
-Usage:
-  hello.py
+USAGE:
+hello.py <input_file> [options]
+
+OPTIONS:
+  -b --bar BAR       Sets bar to BAR. [default: asdf]
+  -f --foo           Sets foo to true.
+  -o --output FILE   Sets output file.
+  -h --help          Show this help.
+  --version          Print version and exit.
 
 """
+
 from docopt import docopt
 
+__author__ = "Andreas Lindhé"
+__license__ = "MIT"
+__version__ = "0.1.0"
+
 if __name__ == '__main__':
-  args = docopt(__doc__, version="test")
+  args = docopt(__doc__, version=__version__)
   print(args)
